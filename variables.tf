4ad0f7621b0d32cf8bddb28ade1d4b9808ee6af3
kwabenasarfo1 <130699678+kwabenasarfo1@users.noreply.github.com>
2023-07-01 21:12:47 +0100

    Update variables.tf_eu

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "eu-west-2"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "jenkins_keypair"
 }
 
 variable "instance_type" {
@@ -24,6 +24,6 @@ variable "tag_name" {
 }
 
 variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  description = "AMI for AML Ec2 instance"
+  default     = "ami-038056f5d3df2259d"
 }
