4dffb6a1e879c9e12ca14dd2a1b19a47d915329e
kwabenasarfo1 <130699678+kwabenasarfo1@users.noreply.github.com>
2023-07-05 13:29:48 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,8 @@ variable "ami_id" {
   description = "AMI for AML Ec2 instance"
   default     = "ami-038056f5d3df2259d"
 }
+
+variable "iam_policy_name" {
+  description = "Allow EC2"
+  default     = "AmazonEC2FullAccess"
+}
